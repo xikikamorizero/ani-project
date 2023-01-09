5e5fabd25086c63ae5144faf56b2eb8bf972d8d4
kln <kogay10987@gmail.com>
2023-01-09 18:39:54 +0500

    updated api

--- src/DALL/api.ts
+++ src/DALL/api.ts
@@ -1,11 +1,26 @@
 import axios from 'axios'
 
-const instance = axios.create({
-    baseURL: 'https://kitsu.io/api/edge'
-})
 
 export const anime = {
-    getAnime(limit=10, offset=1){
-        return axios.get(`https://kitsu.io/api/edge//anime?page[limit]=${limit}&page[offset]=${offset}`).then((response)=>response.data)
+    getAnime(limit = 10, offset = 0) {
+        return axios.get(`https://kitsu.io/api/edge//anime?page[limit]=${limit}&page[offset]=${offset}`).then((response) => response.data)
+    },
+    getAnimeCategories(limit = 10, offset = 0, categories: string) {
+        return axios.get(`https://kitsu.io/api/edge/anime?filter[categories]=${categories}&page[limit]=${limit}&page[offset]=${offset}`)
+    },
+    SearchAnime(limit = 10, offset = 0, name: string) {
+        return axios.get(`https://kitsu.io/api/edge/anime?filter[text]=${name}&page[limit]=${limit}&page[offset]=${offset}`)
+    }
+}
+
+export const manga = {
+    getManga(limit = 10, offset = 0) {
+        return axios.get(`https://kitsu.io/api/edge//manga?page[limit]=${limit}&page[offset]=${offset}`).then((response) => response.data)
+    },
+    getMangaCategories(limit = 10, offset = 0, categories: string) {
+        return axios.get(`https://kitsu.io/api/edge/manga?filter[categories]=${categories}&page[limit]=${limit}&page[offset]=${offset}`)
+    },
+    SearchManga(limit = 10, offset = 0, name: string) {
+        return axios.get(`https://kitsu.io/api/edge/manga?filter[text]=${name}&page[limit]=${limit}&page[offset]=${offset}`)
     }
 }
